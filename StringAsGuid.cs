ffb10eee6510f14d0aad52222b2f724fcfd8fdc8
Javi Mollà <jmolla31@gmail.com>
2020-04-29 22:27:46 +0200

    Initial commit (and hopefully the only one)

---
+++ StringAsGuid.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace System
+{
+    public static class StringAsGuid
+    {
+        public static Guid AsGuid(this string @string)
+        {
+            if (@string == null || @string.Length == 0) return Guid.Empty;
+
+            _ = Guid.TryParse(@string, out Guid parsedGuid);
+
+            return parsedGuid;
+        }
+    }
+}
